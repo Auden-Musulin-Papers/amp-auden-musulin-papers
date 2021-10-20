277c5c58613843ff8a23832a4d0ba49aa628dd98
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-10-20 13:10:54 +0200

    changed stems array to object and added sort function to sort by highest score #4

--- html/js/autocomplete.js
+++ html/js/autocomplete.js
@@ -1,12 +1,13 @@
 openFile = ARCHEapi.openFile;
 
-var stems = [];
+// var stems = [];
+var stemsObj = {};
 openFile("static-search/filenames_auden.txt", (rs) => {
     var filenames = rs.split(',');
     $("#ssForm").find(".ssQueryAndButton").after(
         `<div id="ac-complete"/>`
     );
-    filenames.forEach(function(file) {
+    filenames.forEach(function(file, i) {
         var filename = file.replace('html/','');
         openFile(filename, function(file) {
             const response = JSON.parse(file);
@@ -16,16 +17,20 @@ openFile("static-search/filenames_auden.txt", (rs) => {
             inst.forEach(function(instance) {
                 instances.push(instance.score);   
             });
-            stems.push({
-                score: instances[0],
-                name: stem
-            });            
+            // stems.push(
+            //     {
+            //         name: stem,
+            //         score: instances[0]
+            //     }
+            // );
+            stemsObj[stem] = instances[0];
             // console.log(response);
             // console.log(stem);
             // console.log(json.instances);
         });
     });
     // console.log(stems);
+    // console.log(stemsObj);
 });
 
 const searchInput = $("#ssQuery");
@@ -56,17 +61,26 @@ searchInput.focus(function() {
     function getValue() {
         var inputvalue = searchInput.val().toLowerCase();
 
-        var stemsuggestions = stems.filter(function(stem) {
-            // console.log(stem.name);
-            return stem.name.startsWith(inputvalue);
+        // var stemsuggestions = stems.filter(function(stem) {
+        //     // console.log(stem.name);
+        //     return stem.name.startsWith(inputvalue);
+        // });
+        var stemsuggestions2 = Object.entries(stemsObj).filter(function(stem) {
+            // console.log(stem);
+            return stem[0].startsWith(inputvalue);
         });
-        // console.log(stemsuggestions);
-        stemsuggestions.forEach(function(stemsuggested) {
+        //console.log(stemsuggestions2);
+        var sortedstems = stemsuggestions2.sort(function(a, b) {
+            return a[1] - b[1];
+        }).reverse();
+
+        // console.log(sortedstems);
+        sortedstems.forEach(function(stemsuggested) {
             // console.log(stemsuggested.name + "_" + stemsuggested.score);
             searchInputPanel.append(`
-                <div class="stem row" style="padding:.5em;">
-                    <div class="col-md-10" style="padding-left:1em;">${stemsuggested.name}</div>
-                    <div class="col-md-2">score: ${stemsuggested.score}</div>
+                <div class="stem row" data-subject="${stemsuggested[1]}" style="padding:.5em;">
+                    <div class="col-md-10" style="padding-left:1em;">${stemsuggested[0]}</div>
+                    <div class="col-md-2">score: ${stemsuggested[1]}</div>
                 </div>
             `);
         });
@@ -87,4 +101,8 @@ searchInput.focus(function() {
             searchInput.focus();
         });
     }
+
+    // function sortByScore(stemsuggestions2) {
+    //     return Object.entries(stemsuggestions2).sort((a, b) => (a > b ? -1 : 1));
+    // };
 });
