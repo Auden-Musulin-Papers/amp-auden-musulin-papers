2ad8f1415a5ece95e798f432e553d420ac0fb446
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-11-02 14:57:54 +0100

    staticSearch: autocomplete adapted to wildcards and boolean search, multi word autocomplete added #4

--- html/js/autocomplete.js
+++ html/js/autocomplete.js
@@ -12,6 +12,7 @@ if (localStorage.getItem("auden-staticSearch-ac") !== null) {
     if (now > expiry) {
         localStorage.removeItem("auden-staticSearch-ac");
         download("static-search/filenames_auden.txt");
+        var stems = JSON.parse(localStorage.getItem("auden-staticSearch-ac"));
     }
     var stems = stems.value;
 } else {
@@ -19,8 +20,8 @@ if (localStorage.getItem("auden-staticSearch-ac") !== null) {
     var stems = stemsObj.value;
 }
 
-$("#ssForm").find(".ssQueryAndButton").after(
-    `<div id="ac-complete"/>`
+$("#ssForm").find(".ssQueryAndButton").after(`
+    <div id="ac-complete"/>`
 );
 
 const searchInput = $("#ssQuery");
@@ -46,6 +47,7 @@ searchInput.keyup(function() {
         getValue();
     }
     getItem();
+    getItemMany();
 });
 
 const clearbutton = $("#ssClear");
@@ -57,35 +59,100 @@ clearbutton.click(function() {
 });
 
 function getValue() {
-    var inputvalue = searchInput.val().toLowerCase();
+    var inputvalue = searchInput.val().toLowerCase().replace('"','').replace('+','').replace('-','').replace('"','').replace('*','').replace('?','').replace('[','').replace(']','');
 
-    var stemsuggestions = Object.entries(stems).filter(function(stem) {
-        // console.log(stem);
-        return stem[0].startsWith(inputvalue);            
-    });
-
-    // console.log(stemsuggestions.length);
-    var sortedstems = stemsuggestions.sort(function(a, b) {
-        return a[1] - b[1];
-    }).reverse();
-
-    if (stemsuggestions.length !== 0) {
-        // console.log(sortedstems);
-        sortedstems.forEach(function(stemsuggested) {
-            searchInputPanel.append(`
-                <div class="stem row" style="padding:.5em;">
-                    <div class="s-name col-md-9" style="padding-left:1em;">${stemsuggested[0]}</div>
-                    <div class="col-md-3" style="text-align:right;">score: ${stemsuggested[1]}</div>
-                </div>
-            `);                
-        });
-    } else {
+    if (inputvalue.includes(' ') == true) {
         searchInputPanel.append(`
-            <div class="stem row" style="padding:.5em;">
-                <div class="s-name col-md-9" style="padding-left:1em;">no keywords found</div>
+            <div class="row" id="ac-complete-many">
+                <div class="col-md-12 text-center">
+                    <svg id="ac-select-all" xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                    </svg>
+                </div>
             </div>
         `);
-    } 
+
+        const searchInputPanelMany = $("#ac-complete-many");
+        var inputvalues = inputvalue.split(' ');
+        inputvalues.forEach(function(value) {
+            var idRandom = value + Math.ceil(Math.random() * 10) + Math.ceil(Math.random() * 10) + Math.ceil(Math.random() * 10);
+            var idRandom2 = value + Math.ceil(Math.random() * 10) + Math.ceil(Math.random() * 10);
+            var idRandom3 = value + Math.ceil(Math.random() * 10);
+            searchInputPanelMany.append(`
+                <div class="col-md-4" id="${idRandom}">
+                    <div class="row" style="padding:.25em;">
+                        <div class="col-md-12 stems-many" style="padding:.5em;">
+                            <input id="${idRandom2}" style="max-width:100%;"/>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            var stemsuggestions = Object.entries(stems).filter(function(stem) {
+                // console.log(stem);
+                return stem[0].startsWith(value);            
+            });
+
+            // console.log(stemsuggestions.length);
+            var sortedstems = stemsuggestions.sort(function(a, b) {
+                return a[1] - b[1];
+            }).reverse();  
+
+            var randomCol = $('#' + idRandom);
+            if (stemsuggestions.length !== 0) {
+                // console.log(sortedstems);
+                sortedstems.forEach(function(stemsuggested) {
+                    randomCol.append(`                        
+                        <div class="stem row" style="padding:.25em;">
+                            <div class="s-name-many-${idRandom3} col-md-8" style="padding-left:1em;">${stemsuggested[0]}</div>
+                            <div class="col-md-4" style="text-align:right;"><small>${stemsuggested[1]}</small></div>
+                        </div>
+                    `);         
+                });
+            } else {
+                randomCol.append(`
+                    <div class="row" style="padding:.25em;">
+                        <div class="col-md-12" style="padding-left:1em;">no keywords found</div>
+                    </div>
+                `);
+            }
+            $('.stem .s-name-many-' + idRandom3).on("click", function() {
+                var svalue = $(this).text();
+                // console.log(svalue);
+                var inputMany = $("#" + idRandom2);
+                inputMany.val(svalue);
+            });            
+        });
+
+    } else {
+        var stemsuggestions = Object.entries(stems).filter(function(stem) {
+            // console.log(stem);
+            return stem[0].startsWith(inputvalue);            
+        });
+    
+        // console.log(stemsuggestions.length);
+        var sortedstems = stemsuggestions.sort(function(a, b) {
+            return a[1] - b[1];
+        }).reverse();
+    
+        if (stemsuggestions.length !== 0) {
+            // console.log(sortedstems);
+            sortedstems.forEach(function(stemsuggested) {
+                searchInputPanel.append(`
+                    <div class="stem row" style="padding:.5em;">
+                        <div class="s-name col-md-9" style="padding-left:1em;">${stemsuggested[0]}</div>
+                        <div class="col-md-3" style="text-align:right;"><small>score: ${stemsuggested[1]}</small></div>
+                    </div>
+                `);                
+            });
+        } else {
+            searchInputPanel.append(`
+                <div class="stem row" style="padding:.5em;">
+                    <div class="s-name col-md-9" style="padding-left:1em;">no keywords found</div>
+                </div>
+            `);
+        }
+    }
 
     searchInputPanel.addClass("ac-border");
     searchInput.addClass("ac-border2");
@@ -96,6 +163,19 @@ function getItem() {
         var svalue = $(this).text();
         // console.log(svalue);
         searchInput.val(svalue);
+        searchInput.focus();
+    });
+}
+
+function getItemMany() {
+    $('#ac-select-all').on("click", function() {
+        var svalue = $(".stems-many input");
+        var value = "";
+        for (var i = 0; i < svalue.length; i++) {                    
+            value += svalue[i].value + " ";
+        }
+        // console.log(value);
+        searchInput.val(value);
         searchInput.focus();
     });
 }
