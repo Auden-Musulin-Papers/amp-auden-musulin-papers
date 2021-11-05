8e4edeb4515650601a47e814ea0adf0b0ccdae7c
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-11-05 12:47:44 +0100

    fixed filepath for script.sh autocomplete #4

--- html/autocomplete-addon/script.sh
+++ html/autocomplete-addon/script.sh
@@ -1,9 +1,9 @@
 # bin/bash
 
 echo "create file filenames_auden.txt"
-touch "../static-search/filenames_auden.txt"
-echo "opend dir ../static-search/stems"
-for file in ../static-search/stems/*; do
-    echo $file >> "../static-search/filenames_auden.txt"
+touch "html/static-search/filenames_auden.txt"
+echo "opend dir html/static-search/stems"
+for file in html/static-search/stems/*; do
+    echo $file >> "html/static-search/filenames_auden.txt"
 done
-echo "filenames_auden.txt in ../static-search created"
+echo "filenames_auden.txt in html/static-search created"
